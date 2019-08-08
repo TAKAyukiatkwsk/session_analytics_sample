352e0bc89db07ca01ce6975fb0a4fa69ab56cadc
TAKAyuki_atkwsk <takagi.takayuki.yuuki@gmail.com>
2019-08-08 22:36:32 +0900

    Formatted by rubocop

--- morphological_analytics.rb
+++ morphological_analytics.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'CSV'
 require 'natto'
 
@@ -11,10 +13,14 @@ def read_data(path)
   documents
 end
 
-def parse_with_mecab(nm, text)
-  nodes = nm.enum_parse(text).to_a
+def parse_with_mecab(mecab, text)
+  nodes = mecab.enum_parse(text).to_a
   p nodes
-  nodes.select {|n| !n.is_eos? && n.feature.split(",")[0] == "名詞" && n.feature.split(",")[1] != "非自立"}
+  nodes.select do |n|
+    !n.is_eos? \
+    && n.feature.split(',')[0] == '名詞' \
+    && n.feature.split(',')[1] != '非自立'
+  end
 end
 
 def unigram(word_list)
@@ -25,17 +31,19 @@ def bigram(word_list)
   result = []
   length = word_list.length
   word_list.each_with_index do |word, index|
-    result << word + word_list[index + 1] if index < length -1
+    result << word + word_list[index + 1] if index < length - 1
   end
   result
 end
 
 def count_by_group(list)
-  list.group_by(&:itself).map {|k,v| [k, v.size]}.sort{ |a, b| b[1] <=> a[1] }
+  list.group_by(&:itself)
+      .map { |k, v| [k, v.size] }
+      .sort { |a, b| b[1] <=> a[1] }
 end
 
 data = read_data(ENV['DATA_PATH'])
-parsed_data = data.map {|d| parse_with_mecab(nm, d) }
+parsed_data = data.map { |d| parse_with_mecab(nm, d) }
 unigram_list = parsed_data.map do |pd|
   unigram(pd.map(&:surface))
 end
